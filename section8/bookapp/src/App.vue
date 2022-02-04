567627b82e10f01811fa3de73c226adf24bb0a9c
jin1125 <shogo.jinta@gmail.com>
2022-02-04 19:53:49 +0900

    108

--- section8/bookapp/src/App.vue
+++ section8/bookapp/src/App.vue
@@ -2,7 +2,12 @@
   <v-app>
     <Header/>
       <v-main>
-        <router-view/>
+        <v-container>
+          <router-view
+            :books='books'
+            @add-book-list = 'addBook'
+          />
+        </v-container>
       </v-main>
     <Footer/>
   </v-app>
@@ -11,6 +16,7 @@
 <script>
 import Header from '@/global/Header.vue';
 import Footer from '@/global/Footer.vue';
+ const STORAGE_KEY = 'books'
 
 export default {
   name: 'App',
@@ -19,10 +25,46 @@ export default {
     Header,
     Footer
   },
+  data(){
+    return {
+      books: [],
+      newBook: null
+    }
+  },
+  mounted() {
+    if (localStorage.getItem(STORAGE_KEY)) {
+      try {
+        this.books = JSON.parse(localStorage.getItem(STORAGE_KEY));
+      } catch(e) {
+        localStorage.removeItem(STORAGE_KEY);
+      }
+    }
+  },
+  methods: {
+    addBook(e) {
+      this.books.push({
+        id: this.books.length,
+        title: e.title,
+        description: e.description,
+        readDate: '',
+        memo: ''
 
-  data: () => ({
-    //
-  }),
-
+      });
+      this.saveBooks();
+      // console.log(this.books.slice(-1)[0].id);
+      this.goToEditPage(this.books.slice(-1)[0].id)
+    },
+    removeBook(x) {
+      this.books.splice(x, 1);
+      this.saveBooks();
+    },
+    saveBooks() {
+      const parsed = JSON.stringify(this.books);
+      localStorage.setItem(STORAGE_KEY, parsed);
+    },
+    goToEditPage(id) {
+      this.$router.push(`/edit/${id}`)
+    }
+  }
 };
 </script>
